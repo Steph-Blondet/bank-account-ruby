f5957df2a9f05dde8fba654d7f43743269ea9bc6
Stephanie Blondet <stephanieblondet@Stephanies-iMac.local>
2017-01-22 13:02:28 -0500

    keeping our balance

--- bank_account.rb
+++ bank_account.rb
@@ -19,6 +19,18 @@ class BankAccount
         @transactions.push(description: description, amount: amount)
     end
 
+    def balance
+        balance = 0.0
+        @transactions.each do |transaction|
+            balance += transaction[:amount]
+        end
+        return balance
+    end
+
+    def to_s
+        "Name: #{name}, Balance: #{sprintf("%0.2f", balance)}"
+    end
+
 end
 
 
@@ -26,4 +38,4 @@ end
 bank_account = BankAccount.new("Steph")
 bank_account.credit("Paycheck", 100)
 bank_account.debit("Groceries", 40)
-puts bank_account.inspect
+puts bank_account
